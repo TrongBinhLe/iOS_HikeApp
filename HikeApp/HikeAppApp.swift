c44f78fd9b4d2249ec40c07ad2ca4d2f08ea804c
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-09-05 11:10:15 +0700

    init the hike app by use the swiftUI

---
+++ HikeApp/HikeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeAppApp.swift
+//  HikeApp
+//
+//  Created by admin on 05/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct HikeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
