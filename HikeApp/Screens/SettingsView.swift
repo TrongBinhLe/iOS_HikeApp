ca364a3eee98afdc3cce74957e4e9d581076f06b
TrongBinhLe <ltbinhbk2010@gmail.com>
2024-09-12 18:15:41 +0700

    33.8 make the Alternate app icons for app

--- HikeApp/Screens/SettingsView.swift
+++ HikeApp/Screens/SettingsView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct SettingsView: View {
+    // MARK: PROPERTIES
+    private let alternateAppIcons: [String] = [
+        "AppIcon-MagnifyingGlass",
+        "AppIcon-Map",
+        "AppIcon-Mushroom",
+        "AppIcon-Camera",
+        "AppIcon-Backpack",
+        "AppIcon-Campfire"
+    ]
+    
     var body: some View {
         List {
             // MARK: - SECTION: HEADER
@@ -57,11 +67,50 @@ struct SettingsView: View {
             .listRowSeparator(.hidden)
             // MARK: - SECTION: ICON
             
+            Section {
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 8) {
+                        ForEach(alternateAppIcons.indices, id: \.self) { item in
+                            Button(action: {
+                                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) { error in
+                                    if error != nil {
+                                        print("Failed request to update the app's icon: \(String(describing: error?.localizedDescription))")
+                                    } else {
+                                        print("Success! You have changed the app's icon to \(alternateAppIcons[item])")
+                                    }
+                                }
+                                print("Icon \(alternateAppIcons[item]) press button Icons")
+                                
+                            }, label: {
+                                Image("\(alternateAppIcons[item])-Preview")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 80)
+                                    .cornerRadius(16)
+                            })
+                            .buttonStyle(.borderless)
+                        }
+                    }
+                } //: SCROLL VIEW
+                .padding(.top, 12)
+                
+                Text("Choose your favourite app icon from the collection above.")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.secondary)
+                    .font(.footnote)
+                    .padding(.bottom, 12)
+                
+            } header: {
+                Text("Alternate Icons")
+            } //: SECTION
+            .listRowSeparator(.hidden)
+            
             // MARK: - SECTION: ABOUT
             
             Section {
                 // 1.Basic labeled Content
-//                LabeledContent("Application", value: "Hike")
+                //                LabeledContent("Application", value: "Hike")
                 
                 // 2.Advanced Labeled Content
                 CustomeListRowView(rowLabel: "Application", rowIcon: "apps.iphone", rowContent: "HIKE", rowTintcolor: .blue)
